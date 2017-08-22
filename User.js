b760490383590c19aac286faec42b96a37078303
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-22 20:48:32 +0700

    sign in

--- User.js
+++ User.js
@@ -1,9 +1,27 @@
-class User {
-    static signIn(email, password) {
+const { hash, compare } = require('bcrypt');
+const queryDb = require('./db');
 
+class User {
+    static async signIn(email, rawPassword) {
+        // b1: truy van lay ra email, passwordEnc
+        // b2: compare
+        // Neu ma dang nhap that bai -> catch
+        const selectSql = 'SELECT * FROM "User" WHERE email = $1';
+        const result = await queryDb(selectSql, [email]);
+        if (!result.rows[0]) throw new Error('Email khong ton tai');
+        const { password } = result.rows[0];
+        const same = await compare(rawPassword, password);
+        if (!same) throw new Error('Sai password');
+        return;
     }
 
-    static signUp(email, password, name, phone) {
-
+    static async signUp(email, password, name, phone) {
+        const insertSql = 'INSERT INTO public."User" (email, password, name, phone) VALUES ($1, $2, $3, $4);'
+        const encrypted = await hash(password, 8);
+        return queryDb(insertSql, [email, encrypted, name, phone]);
     }
 }
+
+User.signIn('e', 'x')
+.then(() => console.log('Dang nhap thanh cong'))
+.catch(err => console.log(err.message));
