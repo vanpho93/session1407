d7fbeb32b5b0dc51da6ff487326bd706dbc2e781
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-29 20:30:48 +0700

    session

---
+++ index2.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const parser = require('body-parser').urlencoded({ extended: false });
+const User = require('./User');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => res.render('home'));
+
+app.get('/private', (req, res) => {
+    res.send('Manage your account');
+});
+
+app.get('/dangky', (req, res) => res.render('dangky'));
+app.get('/dangnhap', (req, res) => res.render('dangnhap'));
+
+app.post('/dangky', parser, (req, res) => {
+    const { email, password, name, phone } = req.body;
+    User.signUp(email, password, name, phone)
+    .then(() => res.send('Dang ky thanh cong'))
+    .catch(err => res.send(err.message));
+});
+
+app.post('/dangnhap', parser, (req, res) => {
+    const { email, password } = req.body;
+    User.signIn(email, password)
+    .then(() => res.send('Dang nhap thanh cong'))
+    .catch(err => res.send(err.message));
+});
+
+app.listen(3000, () => console.log('Server started!'));
+
+// Chua dang nhap ma trong /private -> redirect toi /dangnhap
+// Dang nhap roi ma vao /dangnhap /dangky redirect /private
+// Private Hien thi thong tin cua user -> ko truy van db -> Luu trong token
